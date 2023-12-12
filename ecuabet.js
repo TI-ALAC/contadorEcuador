695a1e2d8afc2abf5f98dfead193c6ef8d0658e3
DTI <dti@alacoohperu.pe>
2023-12-12 09:16:32 -0500

    cambio en dias

--- ecuabet.js
+++ ecuabet.js
@@ -14,26 +14,31 @@ init();
 async function getUser() {
 
   // Definir la fecha objetivo (en el formato "YYYY-MM-DDTHH:MM:SS")
-  var fechaObjetivo = new Date("2023-12-10T16:30:00").getTime();
+  const fechaObjetivo = new Date('December 17, 2023 16:30:00').getTime();
 
   // Actualizar la cuenta regresiva cada segundo
   var x = setInterval(function() {
 
-      // Obtener la fecha y hora actual
-      var ahora = new Date().getTime();
-
-      // Calcular la diferencia en milisegundos entre la fecha objetivo y la fecha actual
-      var diferencia = fechaObjetivo - ahora;
-
-      // Convertir los días restantes en horas
-      var horas = Math.floor(diferencia / (1000 * 60 * 60));
-
-      // Calcular minutos y segundos
-      var minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
-      var segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+    const fechaActual = new Date().getTime();
+  
+    // Calcular la diferencia entre la fecha objetivo y la fecha actual
+    let diferencia = fechaObjetivo - fechaActual;
+    
+    // Si quedan más de 24 horas, mostrar solo los días restantes
+    if (diferencia > 86400000) { // 24 horas en milisegundos
+      const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+      var cuentaRegresivaFormateada = `${dias} días`
+    } else {
+      // Convertir el tiempo restante a horas, minutos y segundos
+      const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+      const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+      
+      var cuentaRegresivaFormateada = ('0' + horas).slice(-2) + ':' + ('0' + minutos).slice(-2) + ':' + ('0' + segundos).slice(-2);
+    }
 
       // Formatear la cuenta regresiva en el formato hh:mm:ss
-      var cuentaRegresivaFormateada = ('0' + horas).slice(-2) + ':' + ('0' + minutos).slice(-2) + ':' + ('0' + segundos).slice(-2);
+     
 
       // Mostrar la cuenta regresiva en el elemento con el ID "cuentaRegresiva"
       document.getElementById("cuentaRegresiva").innerHTML = cuentaRegresivaFormateada;
